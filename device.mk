c90743e9010d331b32b7e9b82c9e3aa13b9c9f5f
LucyfMorning <unreadmatheus@gmail.com>
2022-11-18 06:07:05 +0000

    dipper: nuke firmware

--- device.mk
+++ device.mk
@@ -12,9 +12,6 @@ OVERRIDE_PRODUCT_COMPRESSED_APEX := false
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/dipper/dipper-vendor.mk)
 
-# Firmware
-$(call inherit-product, vendor/xiaomi-firmware/dipper/firmware.mk)
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2248
 TARGET_SCREEN_WIDTH := 1080
