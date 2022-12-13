de33dc8effcca851cbc80837bacda94ec44fdde9
Chenyang Zhong <zhongcy95@gmail.com>
2022-12-13 10:00:55 -0500

    Revert "dipper: turn off compressed apex"
    Compressed apex boots fine after some recent kernel fixes.
    
    This reverts commit 6c347ea5dc6467ab1e643b8c21315efb3d10c4df.

--- device.mk
+++ device.mk
@@ -6,8 +6,6 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-OVERRIDE_PRODUCT_COMPRESSED_APEX := false
-
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/dipper/dipper-vendor.mk)
 
