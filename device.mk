ba654a79dc6bcb3c7b048838c793f4ecf06777a1
LucyfMorning <unreadmatheus@gmail.com>
2023-02-12 03:38:11 +0000

    dipper: drop livedisplay

--- device.mk
+++ device.mk
@@ -94,10 +94,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
 
-# Livedisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
